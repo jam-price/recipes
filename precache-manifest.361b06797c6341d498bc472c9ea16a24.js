cde1c4e287b402255c6e0eab1d4f1ef8755022ae
jamesmp88 <jamesmprice@hotmail.co.uk>
2020-10-20 19:04:12 +0100

    Updates

---
+++ precache-manifest.361b06797c6341d498bc472c9ea16a24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a7263f09b481f3ab7a388d2df459e8",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "d09cc88ad5609d77715d",
+    "url": "/recipes/static/css/main.59196370.chunk.css"
+  },
+  {
+    "revision": "cf98864d1891d8882354",
+    "url": "/recipes/static/js/2.56954e41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes/static/js/2.56954e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d09cc88ad5609d77715d",
+    "url": "/recipes/static/js/main.83153cf4.chunk.js"
+  },
+  {
+    "revision": "65aed4a3aaa500d440aa",
+    "url": "/recipes/static/js/runtime-main.9fddb04f.js"
+  }
+]);
